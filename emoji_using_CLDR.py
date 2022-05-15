d5a6675dc934493048981053d9496edfcf848e92
Atman Boulaajaili <aatman.aboulaajala04@gmail.com>
2022-05-15 02:00:33 +0100

    commit on main

---
+++ emoji_using_CLDR.py
@@ -0,0 +1,6 @@
+
+#make sure to put the CLDR short name between \N{ CLDR short name }
+
+print("\N{grinning face}") # grinning face
+ 
+print("\N{winking face}") # winking face
---
+++ emoji_using_emoji_module.py
@@ -0,0 +1,6 @@
+import emoji
+
+# emojize() function requires the CLDR short name as a parameter,
+print(emoji.emojize(":winking_face_with_tongue:")) 
+
+# check README for some popular CLDR short name and their unicode
